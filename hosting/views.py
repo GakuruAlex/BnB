69e0af9b532f021923518b63062171e90035f5e7
alexgicimo <gakurualexgicimo@gmail.com>
2023-04-26 17:16:52 +0000

    Modified create and edit room views to use form from ModelForm instead of forms.Form

--- hosting/views.py
+++ hosting/views.py
@@ -7,7 +7,7 @@ from django.urls  import reverse
 
 def rooms(request):
     rooms= Room.objects.all()
-        
+            
     return render(request,'hosting/display_rooms.html',{'rooms':rooms})
 
 
@@ -24,30 +24,13 @@ def room(request,pk):
 def create_room(request):
     
     if request.method =='POST':
-        form = RoomForm(request.POST)
+        form = RoomForm(request.POST,request.FILES)
         if form.is_valid():
-            image =form.cleaned_data['image_url']
-            name =form.cleaned_data['name']
-            description = form.cleaned_data['description']
-            location = form.cleaned_data['location']
-            price = form.cleaned_data['price']
-            occupied = form.cleaned_data['is_occupied']
-            is_cleaned = form.cleaned_data['is_cleaned']
-            
-            my_room = Room(image=image,
-                           name=name,
-                           description=description,
-                           location=location,
-                           price = price,
-                           occupied=occupied,
-                           is_cleaned = is_cleaned
-                           )
-            
-            my_room.save()
+            form.save()
             return HttpResponseRedirect(reverse('hosting:dashboard'))
     else:
      form = RoomForm()
-     return render(request,'hosting/create_room.html',{'form':form})
+    return render(request,'hosting/create_room.html',{'form':form})
      
      
 def edit_room(request,pk):
@@ -55,29 +38,12 @@ def edit_room(request,pk):
     
     
     if request.method == 'POST':
-        form = RoomForm(request.POST)
+        form = RoomForm(request.POST,request.FILES,instance=room_to_edit)
         if form.is_valid():
-            room_to_edit.image= form.cleaned_data['image_url']
-            room_to_edit.name = form.cleaned_data['name']
-            room_to_edit.description =  form.cleaned_data['description']
-            room_to_edit.location = form.cleaned_data['location']
-            room_to_edit.price = form.cleaned_data['price']
-            room_to_edit.occupied= form.cleaned_data['is_occupied']
-            room_to_edit.is_cleaned =form.cleaned_data['is_cleaned']
-             
-           
-            room_to_edit.save()
+            form.save()            
             return HttpResponseRedirect(reverse('hosting:dashboard'))
     else:
-        form =RoomForm(initial={
-            'image':room_to_edit.image,
-            'name':room_to_edit.name,
-            'description': room_to_edit.description,
-            'location':room_to_edit.location,
-            'price': room_to_edit.price,
-            'occupied':room_to_edit.occupied,
-            'is_cleaned': room_to_edit.is_cleaned
-        })  
+        form =RoomForm(instance=room_to_edit)  
     return render(request,"hosting/edit_room.html",{"form":form,"name":room_to_edit.name})
     
     
