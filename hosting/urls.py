154e3f0d9244a48197be4fc24b7f15f7d5ec443d
alexgicimo <gakurualexgicimo@gmail.com>
2023-05-03 06:58:24 +0000

    Added url for dashboard

--- hosting/urls.py
+++ hosting/urls.py
@@ -4,8 +4,9 @@ from . import views
 app_name='hosting'
 
 urlpatterns = [
-    path('',views.rooms,name="dashboard"),
+    path('',views.rooms,name="homepage"),
     path('view-rooms/',views.rooms,name="view-rooms"),
+    path("view-myrooms/",views.userRooms,name="dashboard"),
     path('createroom/',views.create_room,name="createroom"),
     path('rooms/<int:pk>/',views.room,name="viewroom"),
     path('editroom/<int:pk>/',views.edit_room,name="editroom"),
