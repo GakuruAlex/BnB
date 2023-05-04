afe98a07d2e1007556af289c5a3b2073d3afa207
alexgicimo <gakurualexgicimo@gmail.com>
2023-05-04 09:02:26 +0000

    Added book room form in forms

--- hosting/forms.py
+++ hosting/forms.py
@@ -1,9 +1,24 @@
 from django import forms
-from .models import Room
+from .models import Room,Booking,User
+from django.shortcuts import get_object_or_404
 
-    
     
 class RoomForm(forms.ModelForm):
     class Meta:
         model = Room
         fields = ['image', 'name', 'description', 'location', 'price', 'occupied', 'is_cleaned', 'owner']
+
+
+
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = ['client','room','begin','end']
+        
+        widgets = {
+            'begin': forms.DateInput(attrs={'type': 'date'}),
+            'end': forms.DateInput(attrs={'type': 'date'}),
+        }
+        
+        
