242622d17a9b73112676e65ede5f635e5f4cb22b
alexgicimo <gakurualexgicimo@gmail.com>
2023-05-05 07:01:25 +0000

    Added a url for showing bookings belonging to specific bnb owners

--- hosting/urls.py
+++ hosting/urls.py
@@ -8,14 +8,15 @@ urlpatterns = [
     #rooms urls
     path('',views.rooms,name="homepage"),
     path('view-rooms/',views.rooms,name="view-rooms"),
-    path("view-myrooms/",views.userRooms,name="dashboard"),
-    path('createroom/',views.create_room,name="createroom"),
+    path("view-my-rooms/",views.userRooms,name="dashboard"),
+    path('create-room/',views.create_room,name="createroom"),
     path('rooms/<int:pk>/',views.room,name="viewroom"),
-    path('editroom/<int:pk>/',views.edit_room,name="editroom"),
+    path('edit-room/<int:pk>/',views.edit_room,name="editroom"),
     path('delete/<int:pk>/',views.delete_room, name="delete_room"),
     
     #booking url
     path('book-room/<int:pk>/<int:user_id>/', views.book_room, name='book-room'),
     path("bookings/",views.show_bookings,name="bookings"),
+    path("show-my-bookings/",views.show_my_booking,name="show-my-bookings")
 ]
  
